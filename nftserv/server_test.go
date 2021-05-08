328bf6ffea89f978d67a202536aa8fbf1f017499
Sebastian Stammler <seb@perun.network>
2021-05-08 16:39:24 +0200

    nftserv: Test GET /nft/.../asset and PUT /nft/... endpoints

--- nftserv/server_test.go
+++ nftserv/server_test.go
@@ -3,17 +3,23 @@
 package nftserv_test
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
+	"math/big"
 	"math/rand"
 	"net/http"
+	"os"
+	"path/filepath"
 	"runtime"
+	"strconv"
 	"strings"
 	"testing"
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	ptest "perun.network/go-perun/pkg/test"
 
@@ -27,19 +33,25 @@ import (
 	"github.com/perun-network/nerd-op/nftserv"
 )
 
-const addr = "127.0.0.1:13443"
+const (
+	addr = "127.0.0.1:13443"
+	ext  = "pc"
+)
 
 func TestServer(t *testing.T) {
 	var (
 		require    = require.New(t)
 		rng        = ptest.Prng(t)
 		nfts       = nft.NewMemory()
-		srv        = nftserv.New(nfts, asset.NoStorage{})
-		srverr     = make(chan error, 1)
+		assetsDir  = createTmpAssetsDir(t, ext, 0, 1, 420)
+		assets, _  = asset.NewFileStorage(assetsDir)
+		srv        = nftserv.New(nfts, assets)
 		owner, acc = randomAccount(rng, 5)
 		tv         = acc.Values.OrderedValues()[0]
 		ids        = value.MustAsBigInts(tv.Value)
+		srverr     = make(chan error, 1)
 	)
+	assets.SetExtension(ext)
 
 	go func() {
 		srverr <- srv.ListenAndServe(addr)
@@ -68,10 +80,11 @@ func TestServer(t *testing.T) {
 
 	srv.UpdateBalance(owner, acc)
 
+	// GET /nft/...
 	for _, id := range ids {
 		resp, err := http.Get(url("nft", tv.Token.String(), id))
 		require.NoError(err)
-		require.Equalf(resp.StatusCode, http.StatusOK, "Unexpected status: %s", resp.Status)
+		requireStatus(t, resp, http.StatusOK)
 		var tkn nft.NFT
 		require.NoError(json.NewDecoder(resp.Body).Decode(&tkn))
 		require.Equal(tkn.Token, tv.Token)
@@ -81,15 +94,48 @@ func TestServer(t *testing.T) {
 		require.False(tkn.Secret)
 	}
 
+	// invalid requests
 	expectError := func(geturl string, code int) {
 		resp, err := http.Get(geturl)
 		require.NoError(err)
-		require.Equalf(resp.StatusCode, code, "Unexpected status: %s", resp.Status)
+		requireStatus(t, resp, code)
 	}
 
 	expectError(url("foo"), http.StatusNotFound)
 	expectError(url("nft", eth.NewRandomAddress(rng).String(), ids[0]), http.StatusNotFound)
 	expectError(url("nft", tv.Token.String(), rng.Int()), http.StatusNotFound)
+
+	// GET /nft/.../asset
+	expectAsset := func(token common.Address, id *big.Int, assetId uint) {
+		resp, err := http.Get(url("nft", token.String(), id, "asset"))
+		require.NoError(err)
+		requireStatus(t, resp, http.StatusOK)
+		data, err := io.ReadAll(resp.Body)
+		require.NoError(err)
+		require.Equal(strconv.Itoa(int(assetId)), string(data))
+	}
+
+	tkns := nft.Extract(owner, acc)
+	tkn := &tkns[0]
+	expectAsset(tkn.Token, tkn.ID, 0)
+
+	// PUT /nft/...
+	tkn.AssetID = 420
+	resp, err := putAsJSON(url("nft", tkn.Token.String(), tkn.ID), tkn)
+	require.NoError(err)
+	requireStatus(t, resp, http.StatusOK)
+	expectAsset(tkn.Token, tkn.ID, 420)
+}
+
+func requireStatus(t testing.TB, resp *http.Response, code int) {
+	t.Helper()
+	if code != resp.StatusCode {
+		// only read body if code mismatches; might drain body for later assertion
+		// otherwise.
+		data, err := io.ReadAll(resp.Body)
+		assert.NoErrorf(t, err, "Reading body after status mismatch")
+		t.Fatalf("Unexpected status: %s; Body: %s", resp.Status, string(data))
+	}
 }
 
 func url(elems ...interface{}) string {
@@ -101,6 +147,19 @@ func url(elems ...interface{}) string {
 	return s.String()
 }
 
+func putAsJSON(url string, obj interface{}) (*http.Response, error) {
+	data, err := json.Marshal(obj)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest(http.MethodPut, url, bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	return new(http.Client).Do(req)
+}
+
 func randomAccount(rng *rand.Rand, numNFTs int) (common.Address, tee.Account) {
 	return eth.NewRandomAddress(rng), tee.Account{
 		Nonce: rng.Uint64(),
@@ -109,4 +168,16 @@ func randomAccount(rng *rand.Rand, numNFTs int) (common.Address, tee.Account) {
 			vtest.NewRandomIDSet(rng, numNFTs),
 		),
 	}
+}
+
+func createTmpAssetsDir(t testing.TB, ext string, assetIds ...uint) string {
+	dir := t.TempDir()
+	for _, id := range assetIds {
+		require.NoError(t, os.WriteFile(
+			filepath.Join(dir, fmt.Sprintf("%d.%s", id, ext)),
+			[]byte(strconv.Itoa(int(id))),
+			0666,
+		))
+	}
+	return dir
 }
